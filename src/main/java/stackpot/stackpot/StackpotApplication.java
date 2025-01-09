ad204152312ee80580b0a7da7ec4e6b0ce2c8467
starday119 <jy8294@naver.com>
2025-01-09 16:36:11 +0900

    ? 프로젝트 세팅

---
+++ src/main/java/stackpot/stackpot/StackpotApplication.java
@@ -0,0 +1,13 @@
+package stackpot.stackpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackpotApplication.class, args);
+	}
+
+}
