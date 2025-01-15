44ec08d9cf5290185e7f1c3688402d5e12bbb00d
rudeore-098 <rudeore0928@gmail.com>
2025-01-15 21:38:23 +0900

    [feat/#10] config 등록

--- src/main/java/stackpot/stackpot/config/security/SecurityConfig.java
+++ src/main/java/stackpot/stackpot/config/security/SecurityConfig.java
@@ -1,2 +1,33 @@
-package stackpot.stackpot.config.security;public class SecurityConfig {
+package stackpot.stackpot.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home", "/signup", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
 }
--- src/main/java/stackpot/stackpot/web/controller/MemberViewController.java
+++ src/main/java/stackpot/stackpot/web/controller/MemberViewController.java
@@ -1,2 +1,19 @@
-package stackpot.stackpot.web.controller;public class MemberViewController {
+package stackpot.stackpot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberViewController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+    @GetMapping("/signup")
+    public String signupPage(Model model) {
+//        model.addAttribute("memberJoinDto", new MemberRequestDTO.JoinDto());
+        return "signup";
+    }
+
 }
