5ef85d83c0f724ec36cf10c30079c82f706a9da5
starday119 <jy8294@naver.com>
2025-01-31 02:27:13 +0900

    [MOD]: 팟 시작 시, 온도 올리기 추가

--- src/main/java/stackpot/stackpot/web/dto/CompletedPotDetailResponseDto.java
+++ src/main/java/stackpot/stackpot/web/dto/CompletedPotDetailResponseDto.java
@@ -1,5 +1,6 @@
 package stackpot.stackpot.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,7 +9,10 @@ import lombok.Getter;
 public class CompletedPotDetailResponseDto {
     private Long potId; // 팟 ID
     private String potName; // 팟 이름
+
+    @Schema(description = "팟 시작 날짜 (형식: yyyy. M. d)", example = "2025. 2. 18")
     private String potStartDate; // 시작 날짜
+    @Schema(description = "팟 종료 날짜 (형식: yyyy. M. d)", example = "2025. 3. 5")
     private String potEndDate; // 종료 날짜
     private String potContent; // 팟 설명
     private String potStatus; // 팟 상태
