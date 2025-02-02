7cd73165a6fcd47f0b262e372ea4eff93d200bfc
MiN <81948815+leesumin0526@users.noreply.github.com>
2025-02-02 22:15:42 +0900

    [MOD] 끓인 팟 상세보기 API 수정 (#112)
    * [FEAT] 끓인 팟 상세보기에 사용자 역할 필드 추가

--- src/main/java/stackpot/stackpot/converter/PotDetailConverter.java
+++ src/main/java/stackpot/stackpot/converter/PotDetailConverter.java
@@ -2,6 +2,7 @@ package stackpot.stackpot.converter;
 
 import stackpot.stackpot.domain.Pot;
 import stackpot.stackpot.domain.User;
+import stackpot.stackpot.domain.enums.Role;
 import stackpot.stackpot.web.dto.CompletedPotDetailResponseDto;
 import stackpot.stackpot.web.dto.PotDetailResponseDto;
 import stackpot.stackpot.web.dto.RecruitmentDetailResponseDto;
@@ -9,7 +10,7 @@ import stackpot.stackpot.web.dto.RecruitmentDetailResponseDto;
 import java.util.List;
 
 public interface PotDetailConverter {
-    CompletedPotDetailResponseDto toCompletedPotDetailDto(Pot pot, String appealContent);
+    CompletedPotDetailResponseDto toCompletedPotDetailDto(Pot pot, Role userPotRole, String appealContent);
     PotDetailResponseDto toPotDetailResponseDto(User user, Pot pot, String recruitmentDetails);
 }
 
