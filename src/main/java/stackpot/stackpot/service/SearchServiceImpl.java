6bb1ec9bf53d84857fa6b73cd4afce4343a8ac01
starday119 <jy8294@naver.com>
2025-02-11 00:17:02 +0900

    [MOD]: 검색 응답 수정

--- src/main/java/stackpot/stackpot/service/SearchServiceImpl.java
+++ src/main/java/stackpot/stackpot/service/SearchServiceImpl.java
@@ -15,7 +15,6 @@ import stackpot.stackpot.repository.PotRepository.PotRepository;
 import stackpot.stackpot.web.dto.FeedSearchResponseDto;
 import stackpot.stackpot.web.dto.PotPreviewResponseDto;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,38 +35,27 @@ public class SearchServiceImpl implements SearchService {
 //    }
 @Override
 @Transactional(readOnly = true)
-public List<PotPreviewResponseDto> searchPots(String keyword, Pageable pageable) {
+public Page<PotPreviewResponseDto> searchPots(String keyword, Pageable pageable) {
     Page<Pot> pots = potRepository.searchByKeyword(keyword, pageable);
 
-    if (pots.isEmpty()) {
-        return Collections.emptyList();
-    }
-
-    return pots.stream().map(pot -> {
+    return pots.map(pot -> {
         User user = pot.getUser();
         List<String> recruitmentRoles = pot.getRecruitmentDetails().stream()
                 .map(rd -> rd.getRecruitmentRole().name())
                 .collect(Collectors.toList());
 
         return potConverter.toPrviewDto(user, pot, recruitmentRoles);
-    }).collect(Collectors.toList());
+    });
 }
-
 
     @Override
     @Transactional(readOnly = true)
-    public List<FeedSearchResponseDto> searchFeeds(String keyword, Pageable pageable) {
+    public Page<FeedSearchResponseDto> searchFeeds(String keyword, Pageable pageable) {
         Page<Feed> feeds = feedRepository.findByTitleContainingOrContentContainingOrderByCreatedAtDesc(keyword, keyword, pageable);
 
-        if (feeds.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-        // Page → Stream 변환 후 List로 변환
-        return feeds.getContent().stream()
-                .map(feedConverter::toSearchDto)
-                .collect(Collectors.toList());
+        return feeds.map(feedConverter::toSearchDto);
     }
+
 
 
 }
