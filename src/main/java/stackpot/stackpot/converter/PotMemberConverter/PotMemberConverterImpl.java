3a212e9d0cf3ac2088c512efe9db8422d9327406
starday119 <jy8294@naver.com>
2025-02-18 18:06:27 +0900

    [MOD]: 팟 지원하기 API 닉네임 응답 수정

--- src/main/java/stackpot/stackpot/converter/PotMemberConverter/PotMemberConverterImpl.java
+++ src/main/java/stackpot/stackpot/converter/PotMemberConverter/PotMemberConverterImpl.java
@@ -36,27 +36,12 @@ public class PotMemberConverterImpl implements PotMemberConverter {
                 .userId(entity.getUser().getId())
                 .roleName(roleName)
                 .nickname(nicknameWithRole)
-//                .isOwner(entity.isOwner())
                 .appealContent(entity.getAppealContent())
                 .kakaoId(entity.getUser().getKakaoId())
                 .build();
     }
-//    @Override
-//    public PotMemberInfoResponseDto toKaKaoMemberDto(PotMember entity) {
-//        String roleName = entity.getRoleName() != null ? entity.getRoleName().name() : "멤버";
-//        String nicknameWithRole = entity.getUser().getNickname() + " "+mapRoleName(roleName) ;
-//
-//        return PotMemberInfoResponseDto.builder()
-//
-//                .nickname(nicknameWithRole)
-////                .isOwner(entity.isOwner())
-//                .kakaoId(entity.getUser().getKakaoId())
-//                .potRole(getKoreanRoleName(roleName))
-//                .build();
-//    }
 
 
-    // 팟 생성자 전용 DTO 변환 (유저 테이블에서 역할 가져오기)
     public PotMemberInfoResponseDto toKaKaoCreatorDto(PotMember entity) {
         String creatorRole = mapRoleName(entity.getUser().getRole().name()); // 유저 테이블의 역할 사용
         String nicknameWithRole = entity.getUser().getNickname() + " " + creatorRole;
@@ -70,7 +55,6 @@ public class PotMemberConverterImpl implements PotMemberConverter {
                 .build();
     }
 
-    // 일반 멤버 DTO 변환 (지원 테이블에서 역할 가져오기)
     public PotMemberInfoResponseDto toKaKaoMemberDto(PotMember entity) {
         String roleName = entity.getPotApplication() != null ? entity.getPotApplication().getPotRole().name() : "멤버";
         String nicknameWithRole = entity.getUser().getNickname() + " " + mapRoleName(roleName);
@@ -79,7 +63,7 @@ public class PotMemberConverterImpl implements PotMemberConverter {
                 .potMemberId(entity.getPotMemberId())
                 .nickname(nicknameWithRole)
                 .kakaoId(entity.getUser().getKakaoId())
-                .owner(false) // 일반 멤버는 owner = false
+                .owner(false)
                 .potRole(roleName)
                 .build();
     }
