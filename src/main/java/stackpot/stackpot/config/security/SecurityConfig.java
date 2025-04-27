dd71f25636685516c0ef68d90e1e84cf8d3a89c9
rudeore-098 <rudeore0928@gmail.com>
2025-04-28 00:22:29 +0900

    [FEAT] workflow 추가

--- src/main/java/stackpot/stackpot/config/security/SecurityConfig.java
+++ src/main/java/stackpot/stackpot/config/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/", "/home","/sign-up", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs/**", "/users/oauth/kakao", "/pots","/actuator/health", "/feeds","/reissue").permitAll()
+                        .requestMatchers("/", "/home","/sign-up", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs/**", "/users/oauth/kakao", "/pots","/actuator/health", "/feeds","/reissue","/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, blacklistRepository), UsernamePasswordAuthenticationFilter.class);
