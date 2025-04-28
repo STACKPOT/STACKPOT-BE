1befe7a1dae28df440ead1cad2c1e9508cc31cbd
rudeore-098 <rudeore0928@gmail.com>
2025-04-28 20:55:49 +0900

    [TEST] test

--- src/test/java/stackpot/stackpot/web/controller/HealthCheckControllerTest.java
+++ src/test/java/stackpot/stackpot/web/controller/HealthCheckControllerTest.java
@@ -1,35 +1,35 @@
-//package stackpot.stackpot.web.controller;
-//
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.security.test.context.support.WithMockUser;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-//
-//
-//@WebMvcTest(HealthCheckController.class)
-//class HealthCheckControllerTest {
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private JpaMetamodelMappingContext jpaMetamodelMappingContext;
-//
-//    @Test
-//    @WithMockUser //
-//    @DisplayName("GET /health 요청 시 200 OK와 'ok' 반환")
-//    void shouldReturnOkStatusAndBody() throws Exception {
-//        mockMvc.perform(get("/health"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("ok"));
-//    }
-//}
+package stackpot.stackpot.web.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
+
+@WebMvcTest(HealthCheckController.class)
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JpaMetamodelMappingContext jpaMetamodelMappingContext;
+
+    @Test
+    @WithMockUser //
+    @DisplayName("GET /health 요청 시 200 OK와 'ok' 반환")
+    void shouldReturnOkStatusAndBody() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("ok"));
+    }
+}
