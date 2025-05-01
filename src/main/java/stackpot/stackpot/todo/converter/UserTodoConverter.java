1cb2458f6bfdcb1efcb0b4dc61f5320fc73cb4fd
starday119 <jy8294@naver.com>
2025-05-01 17:42:28 +0900

    refactor: 도메인 기준으로 패키지 구조 변경

--- src/main/java/stackpot/stackpot/todo/converter/UserTodoConverter.java
+++ src/main/java/stackpot/stackpot/todo/converter/UserTodoConverter.java
@@ -4,6 +4,18 @@ import stackpot.stackpot.todo.dto.UserTodoTopMemberDto;
 
 import java.util.List;
 
-public interface UserTodoConverter {
-    List<UserTodoTopMemberDto> toTopMemberDto(List<Object[]> userTodoData);
+import org.springframework.stereotype.Component;
+import java.util.stream.Collectors;
+
+@Component
+public class UserTodoConverter{
+
+    public List<UserTodoTopMemberDto> toTopMemberDto(List<Object[]> userTodoData) {
+        return userTodoData.stream()
+                .map(result -> new UserTodoTopMemberDto(
+                        (Long) result[0], // userId
+                        (Long) result[1]  // todoCount
+                ))
+                .collect(Collectors.toList());
+    }
 }
