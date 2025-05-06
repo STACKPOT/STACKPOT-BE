e1094580932381d768559b3186443550125a6e47
isumin <leesumin0526@gmail.com>
2025-05-06 18:28:02 +0900

    [MOD/#293] - 중복 내용 삭제 및 코드 간소화

--- src/main/java/stackpot/stackpot/badge/service/BadgeService.java
+++ src/main/java/stackpot/stackpot/badge/service/BadgeService.java
@@ -3,7 +3,6 @@ package stackpot.stackpot.badge.service;
 import stackpot.stackpot.badge.entity.Badge;
 
 public interface BadgeService {
-    Badge getBadgeById(Long badgeId);
     Badge getDefaultBadge();
     void assignBadgeToTopMembers(Long potId);
 }
