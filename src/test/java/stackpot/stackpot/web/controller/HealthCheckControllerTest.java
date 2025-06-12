149d4dbe1741c065c37f4c35106923002280da60
Hyun0828 <spdlqj4818@naver.com>
2025-06-12 21:38:47 +0900

    [FIX] HealthCheckControllerTest Security 제외

--- src/test/java/stackpot/stackpot/web/controller/HealthCheckControllerTest.java
+++ src/test/java/stackpot/stackpot/web/controller/HealthCheckControllerTest.java
@@ -3,20 +3,25 @@ package stackpot.stackpot.web.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import stackpot.stackpot.common.controller.HealthCheckController;
+import stackpot.stackpot.config.security.SecurityConfig;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import stackpot.stackpot.common.controller.HealthCheckController;
 
 
-@WebMvcTest(HealthCheckController.class)
+@WebMvcTest(controllers = HealthCheckController.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
+@AutoConfigureMockMvc(addFilters = false)
 class HealthCheckControllerTest {
 
     @Autowired
