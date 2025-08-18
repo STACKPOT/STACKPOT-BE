9e595c9ba1cde2e043ad4f2f370ef025de606727
starday119 <jy8294@naver.com>
2025-08-19 04:02:42 +0900

    [Refactor][jjaeroong]: 닉네임 저장 시 새싹 제외 추가

--- src/main/java/stackpot/stackpot/user/service/UserCommandServiceImpl.java
+++ src/main/java/stackpot/stackpot/user/service/UserCommandServiceImpl.java
@@ -292,7 +292,7 @@ public class UserCommandServiceImpl implements UserCommandService {
         nickname = nickname.trim();
 
         // 채소 이름 리스트
-        String[] vegetables = {"버섯", "양파", "브로콜리", "당근"};
+        String[] vegetables = {"버섯", "양파", "브로콜리", "당근", "새싹"};
 
         for (String vegetable : vegetables) {
             if (nickname.contains(" " + vegetable)) {
