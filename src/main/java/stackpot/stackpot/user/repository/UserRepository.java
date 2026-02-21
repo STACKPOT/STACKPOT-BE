b38b5c75e18c2503ae2d0aee31649e61e2d5174b
starday119 <jy8294@naver.com>
2026-02-21 19:35:18 +0900

    bug/#486: 회원탈퇴 시, 알림 및 피드저장 팟저장 삭제 추가

--- src/main/java/stackpot/stackpot/user/repository/UserRepository.java
+++ src/main/java/stackpot/stackpot/user/repository/UserRepository.java
@@ -1,24 +1,25 @@
 package stackpot.stackpot.user.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import stackpot.stackpot.user.entity.User;
-import stackpot.stackpot.user.entity.enums.Provider;
-import stackpot.stackpot.user.entity.enums.Role;
-
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import io.lettuce.core.dynamic.annotation.Param;
+import stackpot.stackpot.user.entity.User;
+import stackpot.stackpot.user.entity.enums.Provider;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+	Optional<User> findByEmail(String email);
 
-    @Query("select u from User u where u.id = :userId")
-    Optional<User> findByUserId(@Param("userId") Long userId);
+	@Query("select u from User u where u.id = :userId")
+	Optional<User> findByUserId(@Param("userId") Long userId);
 
-    Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
+	Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
 
-    boolean existsByNickname(String nickname);
+	boolean existsByNickname(String nickname);
 
-    @Query("select u.nickname from User u where u.id = :userId")
-    Optional<String> findNameByUserId(@Param("userId") Long userId);
+	@Query("select u.nickname from User u where u.id = :userId")
+	Optional<String> findNameByUserId(@Param("userId") Long userId);
+
 }
